913da8a34080a3ee31c768f6be3f2d30bdcac43f
Oamogetswe Mgidi <51949380+OamoTololo@users.noreply.github.com>
2025-01-23 13:58:31 +0200

    Generated the meeting class and did a get and set for all the properties.

---
+++ src/Entity/Meeting.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MeetingRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=MeetingRepository::class)
+ */
+class Meeting
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $date;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $type;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $notes;
+
+    /**
+     * @ORM\Column(type="json")
+     */
+    private $attendance = [];
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getNotes(): ?string
+    {
+        return $this->notes;
+    }
+
+    public function setNotes(string $notes): self
+    {
+        $this->notes = $notes;
+
+        return $this;
+    }
+
+    public function getAttendance(): ?array
+    {
+        return $this->attendance;
+    }
+
+    public function setAttendance(array $attendance): self
+    {
+        $this->attendance = $attendance;
+
+        return $this;
+    }
+}
