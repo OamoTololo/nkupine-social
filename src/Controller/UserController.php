115156c8643e0e55b47d15ebfa02abb1839cb9a8
Oamogetswe Mgidi <51949380+OamoTololo@users.noreply.github.com>
2025-01-23 18:15:21 +0200

    Created logic to deny user access if they are not the expected user, called the user object to display all the users and see their profile.

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +13,29 @@ class UserController extends AbstractController
     /**
      * @Route("/user", name="app_user")
      */
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        $members = $entityManager->getRepository(User::class)->findAll();
+
         return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
+            'members' => $members,
+        ]);
+    }
+
+    /**
+     * @Route("/profile", name="app_profile")
+     */
+    public function profile(EntityManagerInterface $entityManager): Response
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        $user = $this->getUser();
+        $member = $entityManager->getRepository(User::class)->findOneBy(['email' => $user->getEmail()]);
+
+        return $this->render('user/profile.html.twig', [
+            'member' => $member,
         ]);
     }
 }
