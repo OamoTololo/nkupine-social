f5b851e93099baed94bed4400849b4949f2aa221
Oamogetswe Mgidi <51949380+OamoTololo@users.noreply.github.com>
2025-01-23 18:22:06 +0200

    Created logic for the user to edit their profile and created a form.

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use Cassandra\Type\UserType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -34,8 +36,30 @@ class UserController extends AbstractController
         $user = $this->getUser();
         $member = $entityManager->getRepository(User::class)->findOneBy(['email' => $user->getEmail()]);
 
+
+
         return $this->render('user/profile.html.twig', [
             'member' => $member,
         ]);
+    }
+
+    public function editProfile(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        $user = $this->getUser();
+        $member = $entityManager->getRepository(User::class)->findOneBy(['email' => $user->getEmail()]);
+
+        $form = $this->createForm(UserType::class, $member);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+            $this->addFlash('success', 'Profile updated successfully.');
+
+            return $this->render('user/edit.html.twig', [
+                'form' => $form->createView(),
+            ]);
+        }
     }
 }
